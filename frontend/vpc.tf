380abb059a84ea5079724bc075ebac28193b2176
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-02-25 20:19:13 +0000

    added vpc and eks resources

---
+++ frontend/vpc.tf
@@ -0,0 +1,100 @@
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "10.0.${count.index + 1}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name                              = "eks-private-${count.index + 1}"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "10.0.${count.index + 101}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name                     = "eks-public-${count.index + 1}"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "eks-igw"
+  }
+}
+
+resource "aws_nat_gateway" "eks_nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "eks-nat"
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "eks-nat-eip"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.eks_nat.id
+  }
+
+  tags = {
+    Name = "eks-private-rt"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+
+  tags = {
+    Name = "eks-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
---
+++ terraform/eks and fargate.tf
@@ -0,0 +1,85 @@
+# EKS Cluster
+resource "aws_eks_cluster" "bucketList_backend_cluster" {
+  name     = "my-fargate-cluster"
+  role_arn = aws_iam_role.eks_bucketlist_cluster_role.arn
+  version  = "1.24"
+
+  vpc_config {
+    subnet_ids = concat(aws_subnet.private[*].id, aws_subnet.public[*].id)
+
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_bucketlist_cluster_policy,
+    aws_iam_role_policy_attachment.eks_vpc_resource_controller,
+  ]
+}
+
+# IAM Role for EKS Cluster
+resource "aws_iam_role" "eks_bucketlist_cluster_role" {
+  name = "eks-backetlist-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_bucketlist_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_bucketlist_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_bucketlist_cluster_role.name
+}
+
+# Fargate Profile
+resource "aws_eks_fargate_profile" "bucketList_backend_cluster" {
+  cluster_name           = aws_eks_cluster.bucketList_backend_cluster.name
+  fargate_profile_name   = "default-fargate-profile"
+  pod_execution_role_arn = aws_iam_role.fargate_pod_execution_role.arn
+  subnet_ids             = aws_subnet.private[*].id
+
+  selector {
+    namespace = "default"
+  }
+}
+
+# IAM Role for Fargate Profile
+resource "aws_iam_role" "fargate_pod_execution_role" {
+  name = "eks-fargate-pod-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_pod_execution_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.fargate_pod_execution_role.name
+}
+
+# CoreDNS Addon
+resource "aws_eks_addon" "coredns" {
+  cluster_name      = aws_eks_cluster.bucketList_backend_cluster.name
+  addon_name        = "coredns"
+  addon_version     = "v1.8.7-eksbuild.3"
+  resolve_conflicts = "OVERWRITE"
+
+  depends_on = [aws_eks_fargate_profile.this]
+}
