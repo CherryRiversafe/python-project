0b8b10c3ae659c7a29a862f61f86fe89db4e603b
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-02-27 17:52:40 +0000

    hardcoded alb dns name

--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -17,7 +17,7 @@ const Home = ({userId}) => {
             setLoading(true);
             try {
                 //const response = await axios.get(`${import.meta.VITE_APP_BASE_URL}/get_list/${userId}`);
-                const response = await axios.get(`${process.env.TF_LB_DNS}/get_list/${userId}`);
+                const response = await axios.get(`bucketlist-alb-425435975.eu-west-2.elb.amazonaws.com/get_list/${userId}`);
                 let data = response.data;
                 if (data.length >= 1) {
                     setBucketList(data.map(item => ({ text: item.item, checked: item.checked })));}
