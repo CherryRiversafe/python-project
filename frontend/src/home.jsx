5d22623fae4df7562bd3663fffb0115bbf2e0cd4
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-02-27 17:37:19 +0000

    try a url

--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -16,7 +16,7 @@ const Home = ({userId}) => {
         const get_list = async () => {
             setLoading(true);
             try {
-                const response = await axios.get(`${import.meta.env.TF_LB_DNS}/get_list/${userId}`);
+                const response = await axios.get(`${import.meta.VITE_APP_BASE_URL}/get_list/${userId}`);
                 let data = response.data;
                 if (data.length >= 1) {
                     setBucketList(data.map(item => ({ text: item.item, checked: item.checked })));}
