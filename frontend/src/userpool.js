48fd7bb468dfb06431801f2b0cfade287d54a747
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-02-28 06:05:12 +0000

    updated register url and user pool config

--- frontend/src/userpool.js
+++ frontend/src/userpool.js
@@ -1,7 +1,7 @@
 import {CognitoUserPool} from 'amazon-cognito-identity-js';
 const poolInfo = {
-    UserPoolId: 'eu-west-2_JJZA2kGWs',
-    ClientId: '44ecsojarggv5c00140n1aj4do'
+    UserPoolId: 'eu-west-2_Xk1XPDGP9',
+    ClientId: 'r9qv3o66vcr7oj1575f61if71'
 };
 
 export default new CognitoUserPool(poolInfo)
