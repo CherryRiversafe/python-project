59e0ad210190c77481b20ba9ab83fedb1fa42e69
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-03-05 12:19:47 +0000

    cidrs3

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "eks_vpc" {
 resource "aws_subnet" "private" {
   count             = 2
   vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = "10.0.0.0/19"
+  cidr_block        = "10.0.${count.index + 1}.0/24"
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
   tags = {
@@ -24,7 +24,7 @@ resource "aws_subnet" "private" {
 resource "aws_subnet" "public" {
   count             = 2
   vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = "10.0.0.0/19"
+  cidr_block        = "10.0.${count.index + 1}.0/24"
   availability_zone = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 
