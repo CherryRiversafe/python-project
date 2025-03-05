ff790136412f12f05ca2e4c8c86486f2d1792a73
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-03-05 11:53:52 +0000

    policy change to jsonencode

--- terraform/eks and fargate.tf
+++ terraform/eks and fargate.tf
@@ -22,18 +22,18 @@ resource "aws_eks_cluster" "bucketList_backend_cluster" {
 resource "aws_iam_role" "eks_bucketlist_cluster_role" {
   name = "eks-backetlist-cluster-role"
 
-  assume_role_policy = <<POLICY
+ assume_role_policy = jsonencode({
+  Version = "2012-10-17"
+  Statement = [
     {
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
       Principal = {
         Service = "eks.amazonaws.com"
       }
-    }]
-  }
-POLICY
+    }
+  ]
+})
 }
 
 resource "aws_iam_role_policy_attachment" "eks_bucketlist_cluster_policy" {
