40dd256cda69b9a9ca904383a2fa50ca4b477aaa
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-03-06 19:54:02 +0000

    added node role, compute config, network config and load balancing config

--- terraform/eks and fargate.tf
+++ terraform/eks and fargate.tf
@@ -12,6 +12,28 @@ resource "aws_eks_cluster" "bucketList_backend_cluster" {
     subnet_ids = concat(aws_subnet.private[*].id, aws_subnet.public[*].id)
   }
 
+  access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
+
+  compute_config {
+    enabled = true
+    node_pools    = ["general-purpose"]
+    node_role_arn = aws_iam_role.node.arn
+  }
+
+  kubernetes_network_config {
+    elastic_load_balancing {
+      enabled = true
+    }
+  }
+
+  storage_config {
+    block_storage {
+      enabled = true
+    }
+  }
+
   depends_on = [
     aws_iam_role_policy_attachment.eks_bucketlist_cluster_policy,
     aws_iam_role_policy_attachment.eks_vpc_resource_controller,
@@ -102,4 +124,30 @@ resource "aws_iam_role_policy_attachment" "fargate_pod_execution_role_policy" {
 resource "aws_iam_role_policy_attachment" "fargate_pod_execution_cloudwatch_role_policy" {
   policy_arn = aws_iam_policy.cloudwatch_access.arn
   role       = aws_iam_role.fargate_pod_execution_role.name
+}
+
+resource "aws_iam_role" "node" {
+  name = "eks-auto-node-example"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = ["sts:AssumeRole"]
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodeMinimalPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodeMinimalPolicy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryPullOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+  role       = aws_iam_role.node.name
 }
