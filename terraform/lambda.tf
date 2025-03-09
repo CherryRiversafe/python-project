2dc6c370fa0f79787924758994be90d9183b1af1
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-03-09 20:30:13 +0000

    added lambda terraform config but commented out

---
+++ terraform/lambda.tf
@@ -0,0 +1,44 @@
+# resource "aws_lambda_function" "backend_container" {
+#   function_name = "bucketlist-backend"
+#   role = aws_iam_role.lambda_role.arn
+#   package_type = "Image"
+#   image_uri = "${aws_ecr_repository.ecr_repo}:latest"
+# }
+
+# resource "aws_iam_role" "lambda_role" {
+#     name = "bucketlist-lambda-role"
+
+#     assume_role_policy = jsonencode({
+#         Version = "2012-10-17"
+#         Statement = [
+#             {
+#                 Action = "sts:AssumeRole"
+#                 Effect = "Allow"
+#                 Principal = {
+#                     Service = "lambda.amazonaws.com"
+#                 }
+#             }
+#         ]
+#     })
+# }
+
+# # resource "aws_iam_role_policy" "lambda_role_policy" {
+# #   name = "bucketlist-role-policy"
+# #   role = aws_iam_role.lambda_role.id
+
+# #   policy = jsonencode({
+# #     Version = "2012-10-17"
+# #     Statement = [
+# #         {
+
+# #         }
+# #     ]
+# #   })
+# # }
+
+
+
+# resource "aws_cloudwatch_log_group" "log_group" {
+#     name = "/aws/lambda/${aws_lambda_function.backend_container.name}"
+#     retention_in_days = 14
+# }
