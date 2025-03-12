2259a3dc6ff29685312b8f74a44737d031b5fdd2
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-03-12 19:38:53 +0000

    opened up security group rule for rds

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -70,9 +70,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_postgres_access" {
 resource "aws_vpc_security_group_ingress_rule" "allow_http_access" {
   security_group_id = aws_security_group.rds_sec_group.id
   cidr_ipv4          = "0.0.0.0/0"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
+  ip_protocol       = "-1"
 } 
 
 resource "aws_vpc_security_group_egress_rule" "allow_any_outbound" {
