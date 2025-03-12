b398afcea06ca318f6bee477520c7d26a556635d
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-03-12 22:19:48 +0000

    removed if name main block

--- backend/api.py
+++ backend/api.py
@@ -126,6 +126,6 @@ def delete_item(item_id):
 def handler(event, context):
     return serverless_wsgi.handle_request(app, event, context)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+#if __name__ == '__main__':
+ #   app.run(host='0.0.0.0', port=5000, debug=True)
 
