79a4e09fdad7ef7fbbe2d068fcc7e40a07b4dba0
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-03-13 13:52:40 +0000

    eds back on

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,107 +1,107 @@
-# resource "aws_lambda_function" "backend_container" {
-#   function_name = "bucketlist-backend"
-#   role = aws_iam_role.lambda_role.arn
-#   package_type = "Image"
-#   image_uri = "${aws_ecr_repository.ecr_repo.repository_url}@${var.image_digest}"
-#   timeout = 60
-#   memory_size = 1024
+resource "aws_lambda_function" "backend_container" {
+  function_name = "bucketlist-backend"
+  role = aws_iam_role.lambda_role.arn
+  package_type = "Image"
+  image_uri = "${aws_ecr_repository.ecr_repo.repository_url}@${var.image_digest}"
+  timeout = 60
+  memory_size = 1024
 
-#   environment {
-#     variables = {
-#       db_user_secret_name = aws_secretsmanager_secret.db_user.name
-#       db_password_secret_name = aws_secretsmanager_secret.rds_password_secret.name
-#       rds_endpoint = aws_db_instance.my_bucket_list.address 
-#     }
+  environment {
+    variables = {
+      db_user_secret_name = aws_secretsmanager_secret.db_user.name
+      db_password_secret_name = aws_secretsmanager_secret.rds_password_secret.name
+      rds_endpoint = aws_db_instance.my_bucket_list.address 
+    }
+  }
+
+#    vpc_config {
+#     # Every subnet should be able to reach an EFS mount target in the same Availability Zone. Cross-AZ mounts are not permitted.
+#     subnet_ids         = ["subnet-9826f9e2"]
+#     security_group_ids = [aws_security_group.rds_sec_group.id]
 #   }
-
-# #    vpc_config {
-# #     # Every subnet should be able to reach an EFS mount target in the same Availability Zone. Cross-AZ mounts are not permitted.
-# #     subnet_ids         = ["subnet-9826f9e2"]
-# #     security_group_ids = [aws_security_group.rds_sec_group.id]
-# #   }
-# }
+}
 
 
-# resource "aws_lambda_function_url" "backend_container_url" {
-#   function_name      = aws_lambda_function.backend_container.function_name
-#   authorization_type = "NONE"
+resource "aws_lambda_function_url" "backend_container_url" {
+  function_name      = aws_lambda_function.backend_container.function_name
+  authorization_type = "NONE"
 
-#   cors {
-#     allow_credentials = true
-#     allow_origins     = ["http://bucketlist-frontend-2025.s3-website.eu-west-2.amazonaws.com"]
-#     allow_methods     = ["GET", "POST", "PUT", "DELETE"]
-#     allow_headers     = ["Content-Type"]
-#     expose_headers    = ["Date", "x-amzn-RequestId"]
-#     max_age           = 3600
-#   }
-# }
+  cors {
+    allow_credentials = true
+    allow_origins     = ["http://bucketlist-frontend-2025.s3-website.eu-west-2.amazonaws.com"]
+    allow_methods     = ["GET", "POST", "PUT", "DELETE"]
+    allow_headers     = ["Content-Type"]
+    expose_headers    = ["Date", "x-amzn-RequestId"]
+    max_age           = 3600
+  }
+}
 
 
-# resource "aws_iam_role" "lambda_role" {
-#     name = "bucketlist-lambda-role"
+resource "aws_iam_role" "lambda_role" {
+    name = "bucketlist-lambda-role"
 
-#     assume_role_policy = jsonencode({
-#         Version = "2012-10-17"
-#         Statement = [
-#             {
-#                 Action = "sts:AssumeRole"
-#                 Effect = "Allow"
-#                 Principal = {
-#                     Service = "lambda.amazonaws.com"
-#                 }
-#             }
-#         ]
-#     })
-# }
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    Service = "lambda.amazonaws.com"
+                }
+            }
+        ]
+    })
+}
 
 
 
-# resource "aws_iam_role_policy" "lambda_role_policy" {
-#   name = "bucketlist-role-policy"
-#   role = aws_iam_role.lambda_role.id
+resource "aws_iam_role_policy" "lambda_role_policy" {
+  name = "bucketlist-role-policy"
+  role = aws_iam_role.lambda_role.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#         {
-#             Effect = "Allow"
-#             Action = [
-#                 "logs:CreateLogGroup",
-#                 "logs:CreateLogStream",
-#                 "logs:PutLogEvents"
-#             ]
-#             Resource = [
-#                 "arn:aws:logs:*:*:*"
-#             ]
-#         },
-#         {
-#             Effect = "Allow"
-#             Action = [
-#                 "ec2:CreateNetworkInterface",
-#                 "ec2:DescribeNetworkInterfaces",
-#                 "ec2:DeleteNetworkInterface"
-#             ]
-#             Resource = [
-#                 "*"
-#             ]
-#         },
-#         {
-#             Effect = "Allow"
-#             Action = [
-#                 "secretsmanager:GetSecretValue"
-#             ],
-#             Resource = [
-#                 aws_secretsmanager_secret.rds_password_secret.arn,
-#                 aws_secretsmanager_secret.db_user.arn
-#             ]
-#         }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+        {
+            Effect = "Allow"
+            Action = [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ]
+            Resource = [
+                "arn:aws:logs:*:*:*"
+            ]
+        },
+        {
+            Effect = "Allow"
+            Action = [
+                "ec2:CreateNetworkInterface",
+                "ec2:DescribeNetworkInterfaces",
+                "ec2:DeleteNetworkInterface"
+            ]
+            Resource = [
+                "*"
+            ]
+        },
+        {
+            Effect = "Allow"
+            Action = [
+                "secretsmanager:GetSecretValue"
+            ],
+            Resource = [
+                aws_secretsmanager_secret.rds_password_secret.arn,
+                aws_secretsmanager_secret.db_user.arn
+            ]
+        }
+    ]
+  })
+}
 
 
 
-# resource "aws_cloudwatch_log_group" "log_group" {
-#     name = "/aws/lambda/bucketlist-backend"
-#     retention_in_days = 14
-# }
+resource "aws_cloudwatch_log_group" "log_group" {
+    name = "/aws/lambda/bucketlist-backend"
+    retention_in_days = 14
+}
--- terraform/vpc_rds.tf
+++ terraform/vpc_rds.tf
@@ -1,52 +1,52 @@
-# resource "aws_vpc" "rds_vpc" {
-#   cidr_block           = "10.0.0.0/16"
-#   enable_dns_hostnames = true
-#   enable_dns_support   = true
+resource "aws_vpc" "rds_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-#   tags = {
-#     Name = "rds-vpc"
-#   }
-# }
+  tags = {
+    Name = "rds-vpc"
+  }
+}
 
-# resource "aws_subnet" "public" {
-#   count             = 2
-#   vpc_id            = aws_vpc.rds_vpc.id
-#   cidr_block        = "10.0.${count.index + 101}.0/24"
-#   availability_zone = data.aws_availability_zones.available.names[count.index]
-#   map_public_ip_on_launch = true
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.rds_vpc.id
+  cidr_block        = "10.0.${count.index + 101}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
 
-#   tags = {
-#     Name                     = "rds-public-${count.index + 1}"
-#   }
-# }
+  tags = {
+    Name                     = "rds-public-${count.index + 1}"
+  }
+}
 
-# resource "aws_internet_gateway" "rds_igw" {
-#   vpc_id = aws_vpc.rds_vpc.id
+resource "aws_internet_gateway" "rds_igw" {
+  vpc_id = aws_vpc.rds_vpc.id
 
-#   tags = {
-#     Name = "rds-igw"
-#   }
-# }
+  tags = {
+    Name = "rds-igw"
+  }
+}
 
-# resource "aws_route_table" "public" {
-#   vpc_id = aws_vpc.rds_vpc.id
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.rds_vpc.id
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.rds_igw.id
-#   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.rds_igw.id
+  }
 
-#   tags = {
-#     Name = "rds-public-rt"
-#   }
-# }
+  tags = {
+    Name = "rds-public-rt"
+  }
+}
 
-# resource "aws_route_table_association" "public" {
-#   count          = 2
-#   subnet_id      = aws_subnet.public[count.index].id
-#   route_table_id = aws_route_table.public.id
-# }
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
 
-# data "aws_availability_zones" "available" {
-#   state = "available"
-# }
+data "aws_availability_zones" "available" {
+  state = "available"
+}
