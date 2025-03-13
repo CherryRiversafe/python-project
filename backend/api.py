c90b05dde0654e3d3d325b40dddd05ff81eaca11
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-03-13 07:28:02 +0000

    added return statement

--- backend/api.py
+++ backend/api.py
@@ -125,7 +125,10 @@ def delete_item(item_id):
 
 def handler(event, context):
     print("handler invoked")
-    return serverless_wsgi.handle_request(app, event, context)
+    return {
+        'statusCode': 200
+    }
+    #return serverless_wsgi.handle_request(app, event, context)
 
 #if __name__ == '__main__':
  #   app.run(host='0.0.0.0', port=5000, debug=True)
