3afa1b3caee7db7ddae6f0b432c9d26d9c644555
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-03-13 12:30:43 +0000

    output2

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,12 +1,12 @@
-output "DB_USERNAME" {
-  description = "bucket list db suer name"
-  value       =  aws_secretsmanager_secret.db_user.arn
-}
+# output "DB_USERNAME" {
+#   description = "bucket list db suer name"
+#   value       =  aws_secretsmanager_secret.db_user.arn
+# }
 
-output "DB_PASSWORD" {
-  description = "bucket list db password"
-  value       = aws_secretsmanager_secret.rds_password_secret.name
-}
+# output "DB_PASSWORD" {
+#   description = "bucket list db password"
+#   value       = aws_secretsmanager_secret.rds_password_secret.name
+# }
 
 # output "rds_hostname" {
 #   description = "RDS instance hostname"
