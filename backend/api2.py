233948292ff8740c3d637e521a279c083ba58e0f
AzureAD\CherryZhang <cherry.zhang@riversafe.co.uk>
2025-03-13 16:04:20 +0000

    api add on

--- backend/api2.py
+++ backend/api2.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from sqlalchemy_utils import database_exists, create_database
 import boto3
 import os
-import serverless_wsgi
+#import serverless_wsgi
 
 
 app = Flask(__name__)
@@ -72,12 +72,64 @@ else:
 #     db.create_all()
 #     print("Tables created successfully.")
 
+@app.route('/')
+def home():
+    print('root route invoked')
+    return({'stat_code': 200})
+
+
+@app.route('/add_item/<user_id>', methods=['POST'])
+def add_item(user_id):
+    print('add item called')
+    data = request.json
+    new_item = BucketListTable(id=data['id'], user_id=user_id, item=data['item'], checked=data['checked'])
+    db.session.add(new_item)
+    db.session.commit()
+    print('message : item added successfully')
+    return jsonify({'message': 'item added successfully'})
+
+@app.route('/update_item/<id>', methods=['PUT', 'POST'])
+def update_item(id):
+    print(f'attempting to update item with id {id}')
+    item = BucketListTable.query.filter_by(id=id).first()
+    print(f'retrieved item {item}')
+    if item:
+        data = request.json
+        item.checked = bool(data['checked'])
+        db.session.commit()
+        return 'item updated successfully'
+    return ' not found', 404
+
+
+@app.route('/get_list/<user_id>', methods=['GET'])
+def get_list(user_id):
+    try:
+        items = BucketListTable.query.filter_by(user_id=user_id).all()
+        if items:
+            for item in items:
+                print(item)
+            return jsonify([item.to_dict() for item in items]), 200
+        else:
+            return jsonify({"message": "No items found for this user"}), 200
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return jsonify({"message": "An error occurred"}), 500
+            
+
+@app.route('/delete_item/<item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    print(f'attempting to delete item with id {item_id}')
+    item = BucketListTable.query.filter_by(id = item_id).first()
+    db.session.delete(item)
+    db.session.commit()
+    return {"message": "Item deleted."}, 200
 
 def handler(event, context):
     print("handler invoked")
     return {
         'statusCode': 200
     }
+
     #return serverless_wsgi.handle_request(app, event, context)
 
 #if __name__ == '__main__':
